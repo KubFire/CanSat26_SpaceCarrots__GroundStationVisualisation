799d43152c5f737845058334f0bffc70688d15fc
KubFire <kubfire@gmail.com>
2026-04-13 19:03:02 +0200

    V1.1.1b DummyLoRa change
    Zmenil jsem kodovaci system

--- visualisation_v1/LoRaReceiverDummy/LoRaReceiverDummy.ino
+++ visualisation_v1/LoRaReceiverDummy/LoRaReceiverDummy.ino
@@ -1,14 +1,24 @@
+/*
+LEGENDA SENSOR_MAP:
+M: MILLIS (čas od startu v ms)
+B: TEMP (teplota)
+C: HUM (vlhkost)
+D: ALT (výška)
+E: PRESS (tlak)
+F: LAT (šířka)
+G: LON (délka)
+S: RSSI (síla signálu)
+T: SNR (odstup signál-šum)
+*/
+
 float ground_lat = 49.7950;
 float ground_lon = 16.6800;
 
-// Startovní pozice (100m západně)
-// 100m na západ v těchto zeměpisných šířkách je cca -0.0014 stupně v longitude
 float current_lat = 49.7950;
 float current_lon = 16.6786; 
 
 float altitude = 0;
-float velocity = 0;
-float phase = 0; // 0: Vzestup, 1: Sestup
+float phase = 0; 
 unsigned long startTime;
 
 void setup() {
@@ -18,48 +28,42 @@ void setup() {
 
 void loop() {
   unsigned long currentTime = millis() - startTime;
-  float t = currentTime / 1000.0; // čas v sekundách
+  float t = currentTime / 1000.0;
 
-  // Simulace trajektorie
   if (phase == 0) {
-    // Vzestup do 1km (zjednodušená parabola pro simulaci)
     altitude = 400 * t - 20 * t * t; 
     if (altitude > 1000 || t > 10) {
       altitude = 1000;
       phase = 1;
-      startTime = millis(); // Reset pro fázi sestupu
+      startTime = millis();
     }
-    // Horizontální posun při vzestupu (200m)
     current_lon += 0.000002; 
   } else {
-    // Sestup z 1km na 0 (pomalý pád na padáku)
-    altitude -= 5.0; // 5 m/s klesání
+    altitude -= 5.0;
     if (altitude < 0) altitude = 0;
     
-    // Horizontální posun při sestupu (1km)
     if (altitude > 0) {
       current_lon += 0.00001; 
       current_lat += 0.000002;
     }
   }
 
-  // Generování believable dat
-  float temp = 25.0 - (altitude / 100.0); // Teplota klesá s výškou
+  float temp = 25.0 - (altitude / 100.0);
   float hum = 45.0 + (altitude / 50.0);
   float press = 1013.25 * pow((1 - 0.0000225577 * altitude), 5.25588);
   int rssi = random(-110, -30);
   float snr = random(5, 120) / 10.0;
 
-  // Formátování výstupu dle sensor_map
-  // A:TEMP, B:HUM, C:ALT, D:PRESS, E:LAT, F:LON, R:RSSI, S:SNR
-  Serial.print("A:"); Serial.print(temp, 2);
-  Serial.print(",B:"); Serial.print(hum, 1);
-  Serial.print(",C:"); Serial.print(altitude, 2);
-  Serial.print(",D:"); Serial.print(press, 2);
-  Serial.print(",E:"); Serial.print(current_lat, 6);
-  Serial.print(",F:"); Serial.print(current_lon, 6);
-  Serial.print(",R:"); Serial.print(rssi);
-  Serial.print(",S:"); Serial.println(snr, 1);
+  // Formát: M[ms];B[temp];C[hum];D[alt];E[press];F[lat];G[lon];S[rssi];T[snr]
+  Serial.print("M"); Serial.print(millis());
+  Serial.print(";B"); Serial.print(temp, 2);
+  Serial.print(";C"); Serial.print(hum, 1);
+  Serial.print(";D"); Serial.print(altitude, 2);
+  Serial.print(";E"); Serial.print(press, 2);
+  Serial.print(";F"); Serial.print(current_lat, 6);
+  Serial.print(";G"); Serial.print(current_lon, 6);
+  Serial.print(";S"); Serial.print(rssi);
+  Serial.print(";T"); Serial.println(snr, 1);
 
-  delay(500); // Simulace intervalu LoRa zpráv
+  delay(50);
 }
